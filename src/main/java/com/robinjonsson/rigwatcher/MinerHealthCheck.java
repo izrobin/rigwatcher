52357b728be498ad843aa4bb93de319b0ede3373
Robin Jonsson <robin.jonsson@izettle.com>
2017-12-27 11:08:48 +0100

    Rounded values and new alert value for < 125MH/s

--- src/main/java/com/robinjonsson/rigwatcher/MinerHealthCheck.java
+++ src/main/java/com/robinjonsson/rigwatcher/MinerHealthCheck.java
@@ -1,5 +1,6 @@
 package com.robinjonsson.rigwatcher;
 
+import static com.robinjonsson.rigwatcher.HealthResult.Health.ALMOST_NO_HASHRATE;
 import static com.robinjonsson.rigwatcher.HealthResult.Health.API_ERROR;
 import static com.robinjonsson.rigwatcher.HealthResult.Health.HIGH_STALES;
 import static com.robinjonsson.rigwatcher.HealthResult.Health.LOW_CURRENT_HASHRATE;
@@ -13,9 +14,14 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.setup.Environment;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.client.WebTarget;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,12 +31,16 @@ public class MinerHealthCheck implements Runnable {
 
     private final WebTarget etherpoolTarget;
     private final List<String> recipients = new ArrayList<>();
+
+    private Instant lastHealthy;
+    private boolean reminderAlertSent;
     private HealthResult lastResult;
 
     public MinerHealthCheck(
         final Environment environment,
         final RigWatcherConfig config
     ) {
+        this.lastHealthy = Instant.now();
         this.recipients.addAll(config.getAlertNbrs());
         this.etherpoolTarget = new JerseyClientBuilder(environment)
             .build("EthermineClient")
@@ -48,19 +58,40 @@ public class MinerHealthCheck implements Runnable {
 
         LOG.info("Current health: {}", health);
 
-        if (lastResult == null || health.getHealth() != lastResult.getHealth()) {
-            for (final String recipient : recipients) {
-                final Message alert = Message.creator(
-                    new PhoneNumber(recipient),
-                    new PhoneNumber("RigWatcher"),
-                    health.getMessage()
-                ).create();
+        if (lastResult == null) {
+            lastResult = health;
+        }
 
-                LOG.info("Sent alert to={} message={}", alert.getTo(), alert.getBody());
-            }
+        //Change in health status! Send alerts
+        if (health.getHealth() != lastResult.getHealth()) {
+            sendAlerts(health.getMessage());
+        }
+
+        //We're feeling OK. Let's update our self
+        if (HealthResult.Health.OK == health.getHealth()) {
+            lastHealthy = Instant.now();
+            reminderAlertSent = false;
+        }
+
+        final Instant threeHoursAgo = Instant.now().minus(3, ChronoUnit.HOURS);
+        if (lastHealthy.isBefore(threeHoursAgo) && !reminderAlertSent) {
+            sendAlerts("URGENT! Unhealthy rig status for more than 3 hours! Please take a look");
+            reminderAlertSent = true;
         }
 
         lastResult = health;
+    }
+
+    private void sendAlerts(final String message) {
+        for (final String recipient : recipients) {
+            final Message alert = Message.creator(
+                new PhoneNumber(recipient),
+                new PhoneNumber("RigWatcher"),
+                message
+            ).create();
+
+            LOG.info("Sent alert to={} message={}", alert.getTo(), alert.getBody());
+        }
     }
 
     public HealthResult getHealth() {
@@ -89,7 +120,17 @@ public class MinerHealthCheck implements Runnable {
             return new HealthResult(
                 stats.getLastSeen(),
                 HIGH_STALES,
-                "Warning! High % of stale shares. Stales percentage: " + stats.stalesPercentage()
+                "Warning! High % of stale shares. Stales percentage: " +
+                    round(stats.stalesPercentage())
+            );
+        }
+
+        if (stats.getReportedHashrate() < 125_000_000L) {
+            return new HealthResult(
+                stats.getLastSeen(),
+                ALMOST_NO_HASHRATE,
+                "URGENT! Reported hashrate below 50% of regular value! Reported hashrate (MH/s): " +
+                    round(stats.getReportedHashrate() / 1_000_000L)
             );
         }
 
@@ -98,7 +139,7 @@ public class MinerHealthCheck implements Runnable {
                 stats.getLastSeen(),
                 LOW_REPORTED_HASHRATE,
                 "Warning! Reported hashrate is below 230MH/s. Reported hashrate (MH/s): " +
-                    (stats.getReportedHashrate() / 1_000_000)
+                    round(stats.getReportedHashrate() / 1_000_000)
             );
         }
 
@@ -107,14 +148,18 @@ public class MinerHealthCheck implements Runnable {
                 stats.getLastSeen(),
                 LOW_CURRENT_HASHRATE,
                 "Warning! Average (Last 60min) hashrate is below 190MH/s. Current hashrate (MH/s): " +
-                    (stats.getCurrentHashrate() / 1_000_000)
+                    round(stats.getCurrentHashrate() / 1_000_000)
             );
         }
+
+        final Duration sinceLastHealthy = Duration.between(lastHealthy, Instant.now());
+        DurationFormatUtils.formatDurationHMS(sinceLastHealthy.toMillis());
 
         return new HealthResult(
             stats.getLastSeen(),
             OK,
-            "OK! Status is back to normal."
+            "OK! Status is back to normal. Outage duration: "
+                + DurationFormatUtils.formatDurationHMS(sinceLastHealthy.toMillis())
         );
     }
 
@@ -123,5 +168,12 @@ public class MinerHealthCheck implements Runnable {
             .request()
             .get(EtherpoolMinerStatsWrapper.class)
             .getData();
+    }
+
+    private static double round(final double value) {
+        return BigDecimal
+            .valueOf(value)
+            .setScale(3, BigDecimal.ROUND_UP)
+            .doubleValue();
     }
 }
